80567e873748d3d5f5affd32887411f975d69909
tsubinn@gmail.com <you@example.com>
2020-12-02 21:20:37 +0300

    notFinished

--- admin/test.php
+++ admin/test.php
@@ -1,22 +1,35 @@
 <?
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("test");
-?><?$APPLICATION->IncludeComponent(
-	"bitrix:main.register", 
-	".default", 
+?><?
+
+
+
+?> <? //$APPLICATION->IncludeComponent(
+//	"bitrix:main.register",
+//	".default",
+//	array(
+//		"AUTH" => "N",
+//		"REQUIRED_FIELDS" => array(
+//		),
+//		"SET_TITLE" => "Y",
+//		"SHOW_FIELDS" => array(
+//		),
+//		"SUCCESS_PAGE" => "",
+//		"USER_PROPERTY" => array(
+//		),
+//		"USER_PROPERTY_NAME" => "",
+//		"USE_BACKURL" => "N",
+//		"COMPONENT_TEMPLATE" => ".default"
+//	),
+//	false
+//);?><br>
+ <?$APPLICATION->IncludeComponent(
+	"customComponents:test.class", 
+	"customAjax", 
 	array(
-		"AUTH" => "N",
-		"REQUIRED_FIELDS" => array(
-		),
-		"SET_TITLE" => "Y",
-		"SHOW_FIELDS" => array(
-		),
-		"SUCCESS_PAGE" => "",
-		"USER_PROPERTY" => array(
-		),
-		"USER_PROPERTY_NAME" => "",
-		"USE_BACKURL" => "N",
-		"COMPONENT_TEMPLATE" => ".default"
+		"COMPONENT_TEMPLATE" => "customAjax",
+		"WEB_FORM_ID" => "2"
 	),
 	false
-);?><br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+);?><br><? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
--- local/templates/MY_furniture_dark-blue/components/bitrix/catalog/.default/bitrix/catalog.element/.default/template.php
+++ local/templates/MY_furniture_dark-blue/components/bitrix/catalog/.default/bitrix/catalog.element/.default/template.php
@@ -1,15 +1,14 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
-
 <?$this->SetViewTarget("NAME_Product");?>
 <p><?=$arResult["NAME"]?></p>
 <?$this->EndViewTarget();?>
-
+<?echo($arResult["ORIGINAL_PARAMETERS"]["CURRENT_BASE_PAGE"])?>
 <div class="catalog-detail">
 	<div class="catalog-item">
 <?
 $width = 0;
 if($arParams['DETAIL_SHOW_PICTURE'] != 'N' && (is_array($arResult["PREVIEW_PICTURE"]) || is_array($arResult["DETAIL_PICTURE"]))):
-?>
+    ?>
 		<div class="catalog-item-image">
 <?
 	if(is_array($arResult["DETAIL_PICTURE"])):
@@ -96,5 +95,87 @@ elseif (is_array($arResult['IBLOCK'])):
 <?
 endif;
 ?>
+
+        <a href="#wantСheaperForm"  class="wantСheaper">Хочу дешевле</a>
+
+        <div id="wantСheaperForm"  artnumber="<?=$arResult["PROPERTIES"]["ARTNUMBER"]["VALUE"]?>">
+            <form id="f_want_p">
+                <label for="f_name">Фамилия</label><br>
+                <input type="text" id="LAST_NAME" value="<?=$arResult["USER"]["LAST_NAME"]?>"> <br>
+                <label for="f_name">Имя</label><br>
+                <input type="text" id="NAME" value="<?=$arResult["USER"]["NAME"]?>" ><br>
+                <label for="f_name">Отчество</label><br>
+                <input type="text" id="SECOND_NAME" value="<?=$arResult["USER"]["SECOND_NAME"]?>"><br>
+                <label for="f_name">Телефон</label><br>
+                <input type="text" id="PERSONAL_MOBILE"  value="<?=$arResult["USER"]["PERSONAL_MOBILE"]?>" ><br>
+                <label for="f_name">Желаемая цена</label><br>
+                <input type="text" id="WANT_PRICE" >
+                 <button id="send_want_p">Отправить</button>
+
+            </form>
+        </div>
+
+
 	</div>
 </div>
+
+<script>
+
+    $(".wantСheaper").fancybox();
+
+    $(document).on('click', '#send_want_p', function (e) {
+
+      $(".catalog-item-price").children().remove();
+        $("#f_want_p").submit(function () {
+            return false;
+        });
+        if ($('#comment').val() == "" && $('select').val() == 3) {
+            $('#comment').css('border-color', 'red');
+        } else {
+             var formData = new FormData();
+            formData.append('NAME_PROD', "<?=$arResult['NAME']?>");
+            formData.append('ARTNUMBER', $("#wantСheaperForm").attr("artnumber"));
+            formData.append('NAME', $("#NAME").val());
+            formData.append('LAST_NAME', $("#LAST_NAME").val());
+            formData.append('SECOND_NAME', $("#SECOND_NAME").val());
+            formData.append('PERSONAL_MOBILE', $("#PERSONAL_MOBILE").val());
+            formData.append('USER_ID', '<?=$_SESSION["SESS_AUTH"]["USER_ID"]?>');
+            formData.append('DETAIL_URL', '<? echo $arResult["ORIGINAL_PARAMETERS"]["CURRENT_BASE_PAGE"]?>');
+            formData.append('PRICE', $(".catalog-item-price").html());
+            formData.append('WANT_PRICE',$("#WANT_PRICE").val());
+
+
+
+            for (var pair of formData.entries()) {
+                console.log(pair[0]+ ', ' + pair[1]);
+            }
+            jQuery.ajax({
+                url: '/local/templates/MY_furniture_dark-blue/components/bitrix/catalog/.default/bitrix/catalog.element/.default/ajax/addHighLoad.php',
+                data: formData,
+                cache: false,
+                contentType: false,
+                processData: false,
+                type: 'POST',
+                dataType: 'html',
+                success: function (response) {
+                    console.log(response);
+                    $.fancybox(response, {
+                        // fancybox API options
+                        fitToView: false,
+                        width: 700,
+                        height: 400,
+                        autoSize: true,
+                        closeClick: false,
+                        openEffect: 'none',
+                        closeEffect: 'none'
+                    }); // fancybox
+                    $(document).on('click', '.fancybox-close', function (e) {
+                        location.reload();
+                    });
+                }
+            });
+        }
+    });
+
+
+</script>
